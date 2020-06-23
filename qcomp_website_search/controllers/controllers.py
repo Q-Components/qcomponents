fd6ec696107db6448b781a2d32a9f847dc204906
Zeno Nanon <zna@odoo.com>
2020-06-23 12:35:52 -0700

    [ADD] Module to add more search criteria

---
+++ qcomp_website_search/controllers/controllers.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, http, SUPERUSER_ID, tools, _
+from odoo.http import request
+from odoo.osv import expression
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+class WebsiteSaleSearch(WebsiteSale):
+
+    def _get_search_domain(self, search, category, attrib_values, search_in_description=True):
+        domains = [request.website.sale_product_domain()]
+        if search:
+            for srch in search.split(" "):
+                subdomains = [
+                    [('name', 'ilike', srch)],
+                    [('x_studio_alternate_number', 'ilike', srch)],
+                    [('x_studio_manufacturer', 'ilike', srch)],
+                    [('description_sale', 'ilike', srch)],
+                    [('product_variant_ids.default_code', 'ilike', srch)]
+                ]
+                if search_in_description:
+                    subdomains.append([('description', 'ilike', srch)])
+                    subdomains.append([('description_sale', 'ilike', srch)])
+                domains.append(expression.OR(subdomains))
+
+        if category:
+            domains.append([('public_categ_ids', 'child_of', int(category))])
+
+        if attrib_values:
+            attrib = None
+            ids = []
+            for value in attrib_values:
+                if not attrib:
+                    attrib = value[0]
+                    ids.append(value[1])
+                elif value[0] == attrib:
+                    ids.append(value[1])
+                else:
+                    domains.append([('attribute_line_ids.value_ids', 'in', ids)])
+                    attrib = value[0]
+                    ids = [value[1]]
+            if attrib:
+                domains.append([('attribute_line_ids.value_ids', 'in', ids)])
+
+        return expression.AND(domains)
