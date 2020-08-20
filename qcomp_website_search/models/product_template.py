410f60fc89ea18af46028f3543f11c685e10aa2c
Joren Van Onder <jov@odoo.com>
2020-08-20 16:03:54 -0700

    [FIX] qcomp_website_search: improve /shop search performance
    Make /shop searches faster by not translating these fields. The ORM
    uses CTEs to translate which cannot be efficiently searched.
    
    opw-2295134

---
+++ qcomp_website_search/models/product_template.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+from odoo import models, fields
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    # Make /shop searches faster by not translating these fields. The
+    # ORM uses CTEs to translate which cannot be efficiently
+    # searched.
+    name = fields.Char(translate=False)
+    description = fields.Text(translate=False)
+    description_sale = fields.Text(translate=False)
