6697e238b84d52fbefe6bdca0867ed6273f44c9c
bhautik <vrajatechnologies@gmail.com>
2022-03-29 19:26:05 +0530

    update field

--- qcomponents_sale/models/sale_order.py
+++ qcomponents_sale/models/sale_order.py
@@ -16,4 +16,4 @@ class SaleOrder(models.Model):
 class ProductProduct(models.Model):
     _inherit = "product.product"
     
-    x_studio_field_2dpeg = fields.Float(string='Quotation Price')
+    x_studio_field_2dpeg = fields.Float(string='Quotation Price', digits='Product Price')
