371ce4c0b0dfb30afc5364997ea178cee7c686a1
Jimson Ma <jimm@odoo.com>
2023-03-29 13:21:38 -0700

    [FIX]Updated version number in manifest.

--- qcomponents_sale/__manifest__.py
+++ qcomponents_sale/__manifest__.py
@@ -21,7 +21,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Custom Development',
-    'version': '15.0.0',
+    'version': '16.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['sale', 'product', 'contacts'],
