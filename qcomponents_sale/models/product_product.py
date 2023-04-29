37ca53386ef4396e6fe2dcc8d983b7618409cfe7
Bhautik <support@vrajatechnologies.com>
2023-04-29 17:33:14 +0530

    product fields added

--- qcomponents_sale/models/product_product.py
+++ qcomponents_sale/models/product_product.py
@@ -4,3 +4,6 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     x_studio_field_2dpeg = fields.Float(string='Quotation Price', digits='Product Price')
+    sku_location = fields.Char(string='Sku Location')
+    x_studio_condition_1 = fields.Char(string='Condition')
+    supplier_name = fields.Char(string='Supplier Name')
