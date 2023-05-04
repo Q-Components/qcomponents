a1db8731831dbb793e58ca0f3d7d6a0728b35940
Jimson Ma <jimm@odoo.com>
2023-05-04 14:41:14 -0700

    [FIX]Undid modifications to Odoo modules, fixed quotation_price field in product.

--- qcomponents_sale/models/product_product.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models, _
-
-class ProductProduct(models.Model):
-    _inherit = "product.product"
-
-    x_studio_field_2dpeg = fields.Float(string='Quotation Price', digits='Product Price')
-    sku_location = fields.Char(string='Sku Location')
-    x_studio_condition_1 = fields.Char(string='Condition')
-    supplier_name = fields.Char(string='Supplier Name')
