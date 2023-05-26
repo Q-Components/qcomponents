6cad8349a7d8d94e3d5cefb37859355d7d75588b
Bhautik <support@vrajatechnologies.com>
2023-05-26 12:27:18 +0530

    added invoice button

--- qcomponents_sale/models/stock_picking.py
+++ qcomponents_sale/models/stock_picking.py
@@ -5,6 +5,8 @@ from datetime import datetime
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
+    account_invoice_ids = fields.Many2many('account.move', string="Account Invoice", copy=False)
+
     def generate_account_payment(self, invoice_obj):
         if invoice_obj.move_type == 'out_invoice':
             payment_type = 'inbound'
@@ -44,6 +46,47 @@ class StockPicking(models.Model):
         for picking in self:
             if picking.location_dest_id.id == partner_location:
                 inv_id = picking.sale_id._create_invoices()
+                self.account_invoice_ids = [(4, inv_id.id)]
                 inv_id.with_user(1).action_post()
                 self.with_user(1).generate_account_payment(inv_id)
         return res
+    def action_view_account_invoice(self):
+        if self.sale_id:
+            invoices = self.mapped('account_invoice_ids')
+            action = self.env.ref('account.action_move_out_invoice_type').read()[0]
+            if len(invoices) > 1:
+                action['domain'] = [('id', 'in', invoices.ids)]
+            elif len(invoices) == 1:
+                action['views'] = [(self.env.ref('account.view_move_form').id, 'form')]
+                action['res_id'] = invoices.ids[0]
+            else:
+                action = {'type': 'ir.actions.act_window_close'}
+            return action
+        elif self.purchase_id:
+            action = self.env.ref('account.action_move_in_invoice_type')
+            result = action.read()[0]
+            if self.purchase_id:
+                result['context'] = {'type': 'in_invoice', 'default_purchase_id': self.purchase_id.id}
+            if self.sale_id:
+                result['context'] = {'type': 'out_invoice', 'default_sale_id': self.sale_id.id}
+
+            if not self.account_invoice_ids:
+                journal_domain = [
+                    ('type', '=', 'purchase'),
+                    ('company_id', '=', self.company_id.id),
+                    ('currency_id', '=', self.purchase_id.currency_id.id),
+                ]
+                default_journal_id = self.env['account.journal'].search(journal_domain, limit=1)
+                if default_journal_id:
+                    result['context']['default_journal_id'] = default_journal_id.id
+            else:
+                # Use the same account journal than a previous invoice
+                result['context']['default_journal_id'] = self.account_invoice_ids.journal_id.id
+
+            if len(self.account_invoice_ids) != 1:
+                result['domain'] = "[('id', 'in', " + str(self.account_invoice_ids) + ")]"
+            elif len(self.account_invoice_ids) == 1:
+                res = self.env.ref('account.view_move_form', False)
+                result['views'] = [(res and res.id or False, 'form')]
+                result['res_id'] = self.account_invoice_ids.id
+            return result
