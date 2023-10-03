94c7a60fa47ed70eb7fb8e512eb9c69930ed1a91
Bhautik <support@vrajatechnologies.com>
2023-10-03 13:28:58 +0530

    quick shop search option

---
+++ wr_website_customisation/controller/main.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+import math
+from odoo import fields, http, _
+from odoo.http import request
+
+
+class WebsiteSale(http.Controller):
+
+    @http.route('/quick/shop', type='http', auth="public", website=True)
+    def website_quick_shop(self, **kw):
+        return request.render("wr_website_customisation.QuickShop", {})
+
+    # def _prepare_quick_shop_domain(self, term):
+    #     website = request.env['website'].get_current_website()
+    #     return ['&', ('sale_ok', '=', True), ('website_id', 'in', [False, website.id]),
+    #             '|', '|', '|', ('name', 'ilike', term), ('default_code', 'ilike', term),
+    #             ('description', 'ilike', term), ('x_studio_alternate_number', 'ilike', term)]
+
+    @http.route(['/fetch_quick_shop_products'], type='json', auth="public", website=True, sitemap=False)
+    def fetch_quick_shop_products(self, **post):
+        if 'term' in post:
+            # domain = self._prepare_quick_shop_domain(post.get('term'))
+            # product_fields = ['name', 'x_studio_alternate_number', 'qty_available', 'description_sale',
+            #                   'uom_id', 'website_url']
+            # products = request.env['product.product'].sudo().search_read(domain=domain,
+            #                                                              fields=product_fields,
+            #                                                              offset=post.get('offset', 0),
+            #                                                              limit=20)
+            request.env.cr.execute(f"""
+                select pp.id from product_product pp
+                join product_template pt
+                on pp.product_tmpl_id = pt.id
+                where pt.sale_ok = 't'
+                and (pt.website_id = 1 or pt.website_id is null )
+                and (pt.name ILIKE '%{post.get('term')}%'
+                or pt.x_studio_alternate_number ilike '%{post.get('term')}%'
+                or pt.default_code ilike '%{post.get('term')}%') 
+                limit {post.get('limit', 20)} OFFSET {post.get('offset', 0)}
+            """)
+            products_ids = request.env.cr.fetchall()
+            products_ids = [product[0] for product in products_ids]
+            products_ids = request.env['product.product'].sudo().browse(products_ids)
+            products = [{
+                'id': product_id.id,
+                'name': product_id.name,
+                'x_studio_alternate_number': product_id.x_studio_alternate_number,
+                'qty_available': product_id.qty_available,
+                'description_sale': product_id.description_sale,
+                'uom_id': [product_id.uom_id.id, product_id.uom_id.name],
+                'website_url': product_id.website_url,
+                'price': product_id.lst_price,
+            } for product_id in products_ids]
+            request.env.cr.execute(f"""
+                select count(pp.id) from product_product pp
+                join product_template pt
+                on pp.product_tmpl_id = pt.id
+                where pt.sale_ok = 't'
+                and (pt.website_id = 1 or pt.website_id is null )
+                and (pt.name ILIKE '%{post.get('term')}%'
+                or pt.x_studio_alternate_number ilike '%{post.get('term')}%'
+                or pt.default_code ilike '%{post.get('term')}%')
+            """)
+            total_products = request.env.cr.fetchall()
+            total_products = total_products[0][0]
+            max_offset = math.ceil(total_products / post.get('limit') or 1)
+            max_offset = (max_offset - 1) * post.get('limit')
+            return {
+                'success': True,
+                'products': products,
+                'next_offset': (post.get('offset') + post.get('limit')),
+                'current_offset': post.get('offset') + 1,
+                'max_offset': max_offset,
+                'total_products': total_products
+            }
+        return {
+            'success': False,
+            'products': []
+        }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ wr_website_customisation/static/src/js/quick_shop.js
@@ -0,0 +1,147 @@
+odoo.define('wr_website_customisation.quick_shop', function(require) {
+'use strict';
+
+    var ajax = require('web.ajax');
+	var Widget = require('web.Widget');
+	var Dialog = require('web.Dialog');
+	var rpc = require('web.rpc');
+	var core = require('web.core');
+
+	var QWeb = core.qweb;
+	var _t = core._t;
+
+    var QuickShopTemplate = Widget.extend({
+        template: 'QuickShopTemplate',
+        events: {
+            'click .pagination-prev-btn':'_clickPaginationPrevButton',
+            'click .pagination-next-btn':'_clickPaginationNextButton',
+            'keyup input[type="search"]': '_keyupSearchProduct',
+            'click a.js_decrease_qty': '_clickDecreaseQty',
+            'click a.js_increase_qty': '_clickIncreaseQty',
+            'click a.add_to_cart': '_clickAddToCart',
+            'click button.term_search_btn': '_clickTermSearch',
+        },
+        init: function(parent, options){
+            this.data = [];
+            this.term = '';
+            this.limit = 20;
+            this.offset = 0;
+            this._super.apply(this, arguments);
+        },
+        willStart: function(){
+        	return $.when(this.load_quick_shop_products());
+        },
+        start: function(){
+            return this._super.apply(this, arguments).then(function(){
+                $(".website-loader").fadeOut("slow");
+                $('[type="search"]').focus();
+            });
+        },
+        load_quick_shop_products: function(){
+            var self = this;
+            return rpc.query({
+                route: '/fetch_quick_shop_products',
+                params: {
+                    'term': this.term,
+                    'limit': this.limit,
+                    'offset': this.offset,
+                }
+            }).then(function(result){
+                if(result && result.success){
+                    self.data = result;
+                }
+            });
+        },
+        _clickPaginationPrevButton: function(ev){
+            ev.preventDefault();
+            var self = this;
+            if(this.data && this.data.current_offset > 1){
+                this.offset = (this.data.current_offset - 1) - this.limit;
+            }
+            this.load_quick_shop_products().then(function(){
+                self.renderElement();
+            });
+        },
+        _clickPaginationNextButton: function(ev){
+            var self = this;
+            ev.preventDefault();
+            if(this.data && this.data.next_offset <= this.data.max_offset){
+                this.offset = this.data.next_offset;
+            }
+            this.load_quick_shop_products().then(function(){
+                self.renderElement();
+            });
+        },
+        _keyupSearchProduct: function(ev){
+            var self = this;
+            ev.preventDefault();
+            if(ev.keyCode === 13){
+                if($(ev.currentTarget).val().length <= 0){
+                    this.term = '';
+                }else{
+                    this.term = $(ev.currentTarget).val();
+                }
+                this.load_quick_shop_products().then(function(){
+                    self.renderElement();
+                });
+            }
+        },
+        _clickIncreaseQty: function(ev){
+            ev.preventDefault();
+            $('input[name="product_qty"]').val(Number($('input[name="product_qty"]').val() || 0) + 1);
+        },
+        _clickDecreaseQty: function(ev){
+            ev.preventDefault();
+            if(Number($('input[name="product_qty"]').val()) > 0) {
+                $('input[name="product_qty"]').val(Number($('input[name="product_qty"]').val() || 0) - 1);
+            }
+        },
+        _clickAddToCart: function(ev){
+            ev.preventDefault();
+            var self = this;
+            var product_id = Number($(ev.currentTarget).data('product-id'));
+            var value = Number($('input[name="product_qty"]').val());
+            if(product_id && value > 0){
+                $(".website-loader").fadeIn("slow");
+                rpc.query({
+                    route: "/shop/cart/update_json",
+                    params: {
+                        line_id: false,
+                        product_id: product_id,
+                        set_qty: value
+                    },
+                }).then(function (data) {
+                    if(data.cart_quantity > 0){
+                        $('.my_cart_quantity').text(data.cart_quantity);
+                    }else{
+                        if(data.warning){
+                            Dialog.alert(self, _t(data.warning));
+                        }
+                    }
+                    $(".website-loader").fadeOut("slow");
+                });
+            }
+        },
+        _clickTermSearch: function(ev){
+            var self = this;
+            ev.preventDefault();
+            if($(ev.currentTarget).parent().find('input[type="search"]').val().length <= 0){
+                this.term = '';
+            }else{
+                this.term = $(ev.currentTarget).parent().find('input[type="search"]').val();
+            }
+            this.load_quick_shop_products().then(function(){
+                self.renderElement();
+            });
+        }
+	});
+
+	$(document).ready(function(){
+	    if(location.pathname == '/quick/shop'){
+            var $elem = $('.oe_quick_shop_view');
+            var quick_shop_view = new QuickShopTemplate(null, $elem);
+            quick_shop_view.appendTo($elem);
+	    }
+    });
+
+});
