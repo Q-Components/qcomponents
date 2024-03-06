95b3b17778cb6761daeea64657b14b23ad51b451
jethwajeet <jeetj@vrajatechnologies.com>
2024-03-06 17:11:02 +0530

    Testing module

--- vraja_shopify_odoo_integration/controller/main.py
+++ vraja_shopify_odoo_integration/controller/main.py
@@ -46,19 +46,17 @@ class Main(http.Controller):
         if res.get("first_name") and res.get("last_name"):
             _logger.info("%s call for Customer: %s", webhook_route,
                          (res.get("first_name") + " " + res.get("last_name")))
-            self.customer_webhook_process(instance)
+            self.customer_webhook_process(instance, res)
         return
 
-    def customer_webhook_process(self, instance):
+    def customer_webhook_process(self, instance_id, res):
         """
         This method used for call child method of customer create process.
         """
-        time.sleep(10)
-        to_date = datetime.now()
-        from_date = to_date - timedelta(days=10)
-        create_customer_queue = request.env["customer.data.queue"].sudo()
-        create_customer_queue.import_customers_from_shopify_to_odoo(instance, from_date, to_date)
-        return True
+        _logger.info("shopify Customer response :: {}".format(res))
+        queue_id = request.env['customer.data.queue'].sudo().generate_shopify_customer_queue(instance_id)
+        request.env['customer.data.queue.line'].sudo().create_shopify_customer_queue_line(res, instance_id,
+                                                                                          queue_id)
 
     @http.route("/shopify_odoo_webhook_for_orders_create", csrf=False, auth="public", type="json")
     def order_create_webhook(self):
@@ -83,7 +81,7 @@ class Main(http.Controller):
         _logger.info("shopify order response :: {}".format(res))
         queue_id = request.env['order.data.queue'].sudo().generate_shopify_order_queue(instance_id)
         request.env['order.data.queue.line'].sudo().create_shopify_order_queue_line(res, instance_id,
-                                                                             queue_id)
+                                                                                    queue_id)
 
     def get_basic_info(self, route):
         """
