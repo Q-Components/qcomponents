e5e3b236931be5a7630ecfa968f61654d63bcde8
Bhautik <support@vrajatechnologies.com>
2024-03-12 11:46:09 +0530

    added sitemap controller

--- wr_website_customisation/controller/main.py
+++ wr_website_customisation/controller/main.py
@@ -3,7 +3,24 @@
 import math
 from odoo import fields, http, _
 from odoo.http import request
+from odoo.addons.website.controllers.main import Website
 
+import base64
+import datetime
+import logging
+
+from itertools import islice
+
+
+import odoo
+from odoo.http import request, SessionExpiredException
+
+logger = logging.getLogger(__name__)
+
+# Completely arbitrary limits
+MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT = IMAGE_LIMITS = (1024, 768)
+LOC_PER_SITEMAP = 45000
+SITEMAP_CACHE_TIME = datetime.timedelta(hours=12)
 
 class WebsiteSale(http.Controller):
 
@@ -84,3 +101,76 @@ class WebsiteSale(http.Controller):
             'products': []
         }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+class web_inherit(Website):
+    @http.route('/sitemap.xml', type='http', auth="public", website=True, multilang=False, sitemap=False)
+    def sitemap_xml_index(self, **kwargs):
+        logger.info("Controller Called")
+        current_website = request.website
+        Attachment = request.env['ir.attachment'].sudo()
+        View = request.env['ir.ui.view'].sudo()
+        mimetype = 'application/xml;charset=utf-8'
+        content = None
+
+        def create_sitemap(url, content):
+            return Attachment.create({
+                'raw': content.encode(),
+                'mimetype': mimetype,
+                'type': 'binary',
+                'name': url,
+                'url': url,
+            })
+
+        dom = [('url', '=', '/sitemap-%d.xml' % current_website.id), ('type', '=', 'binary')]
+        sitemap = Attachment.search(dom, limit=1)
+        if sitemap:
+            # Check if stored version is still valid
+            create_date = fields.Datetime.from_string(sitemap.create_date)
+            delta = datetime.datetime.now() - create_date
+            if delta < SITEMAP_CACHE_TIME:
+                content = base64.b64decode(sitemap.datas)
+
+        if not content:
+            # Remove all sitemaps in ir.attachments as we're going to regenerated them
+            dom = [('type', '=', 'binary'), '|', ('url', '=like', '/sitemap-%d-%%.xml' % current_website.id),
+                   ('url', '=', '/sitemap-%d.xml' % current_website.id)]
+            sitemaps = Attachment.search(dom)
+            sitemaps.unlink()
+
+            pages = 0
+            locs = request.website.with_user(request.website.user_id)._enumerate_pages()
+            while True:
+                values = {
+                    'locs': islice(locs, 0, LOC_PER_SITEMAP),
+                    'url_root': request.httprequest.url_root[:-1],
+                }
+                logger.info("Pages :{}".format(locs))
+                urls = View._render_template('website.sitemap_locs', values)
+                logger.info("Values :{}".format(values))
+                if urls.strip():
+                    content = View._render_template('website.sitemap_xml', {'content': urls})
+                    pages += 1
+                    last_sitemap = create_sitemap('/sitemap-%d-%d.xml' % (current_website.id, pages), content)
+                else:
+                    break
+
+            if not pages:
+                return request.not_found()
+            elif pages == 1:
+                # rename the -id-page.xml => -id.xml
+                last_sitemap.write({
+                    'url': "/sitemap-%d.xml" % current_website.id,
+                    'name': "/sitemap-%d.xml" % current_website.id,
+                })
+            else:
+                # TODO: in master/saas-15, move current_website_id in template directly
+                pages_with_website = ["%d-%d" % (current_website.id, p) for p in range(1, pages + 1)]
+
+                # Sitemaps must be split in several smaller files with a sitemap index
+                content = View._render_template('website.sitemap_index_xml', {
+                    'pages': pages_with_website,
+                    'url_root': request.httprequest.url_root,
+                })
+                create_sitemap('/sitemap-%d.xml' % current_website.id, content)
+
+        return request.make_response(content, [('Content-Type', mimetype)])
