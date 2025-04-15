e31181cb49f9d63d864b1821e1a84b79e2a97628
Bhautik <support@vrajatechnologies.com>
2025-04-15 11:03:09 +0530

    check the last internal date

--- advanced_email_configurator/models/fetchmail.py
+++ advanced_email_configurator/models/fetchmail.py
@@ -103,7 +103,8 @@ class FetchmailServer(models.Model):
                     imap_server.store(num, '-FLAGS', '\\Seen')
                 self._cr.commit()
                 count += 1
-                last_date = date_uids[num] or False
+                last_date = date_uids[num] or False #datetime.now() or 
+                _logger.info("Last Date : {}".format(last_date))
                 if last_date:
                     vals = {'last_internal_date': last_date}
                     self.write(vals)
