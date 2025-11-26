73e7a17ed637b6adfdec1dfc2b922ac27b143596
mansi <mansiv@vrajatechnologies.com>
2025-11-26 15:31:38 +0530

    [FIX]website checkout module issue.

--- website_sale_checkout_limit/controllers/controllers.py
+++ website_sale_checkout_limit/controllers/controllers.py
@@ -1,29 +1,37 @@
 # -*- coding: utf-8 -*-
 from odoo.addons.website_sale.controllers.main import WebsiteSale
-
 from odoo import http
 from odoo.http import request
 
 
 class WebsiteSaleExtended(WebsiteSale):
+
+    def _get_order(self):
+        """Get current sale order for the session."""
+        return request.website.get_current_website_sale_order()
+
+    def _check_min_amount(self, order):
+        """Return True if order meets minimum amount, else False."""
+        MIN_AMOUNT = 100  # Replace with your minimum cart amount
+        return order and order.amount_total >= MIN_AMOUNT
+
     @http.route(['/shop/cart/update_json'], type='json', auth="public", methods=['POST'], website=True, csrf=False)
     def cart_update_json(self, product_id, line_id=None, add_qty=None, set_qty=None, display=True):
-        result = super(WebsiteSaleExtended, self).cart_update_json(product_id, line_id, add_qty, set_qty, display)
-
-        is_min = request.website.check_cart_amount()
-        result['website_sale.check'] = is_min
+        result = super().cart_update_json(product_id, line_id, add_qty, set_qty, display)
+        order = self._get_order()
+        result['website_sale.check'] = self._check_min_amount(order)
         return result
 
     @http.route(['/shop/checkout'], type='http', auth="public", website=True, sitemap=False)
     def checkout(self, **post):
-        is_min = request.website.check_cart_amount()
-        if is_min:
-            return super(WebsiteSaleExtended, self).checkout(**post)
+        order = self._get_order()
+        if self._check_min_amount(order):
+            return super().checkout(**post)
         return request.redirect("/shop/cart")
 
     @http.route('/shop/payment', type='http', auth='public', website=True, sitemap=False)
     def shop_payment(self, **post):
-        is_min = request.website.check_cart_amount()
-        if is_min:
-            return super(WebsiteSaleExtended, self).shop_payment(**post)
+        order = self._get_order()
+        if self._check_min_amount(order):
+            return super().shop_payment(**post)
         return request.redirect("/shop/cart")
