71aed910c9acbad217970e97fc390c29cad36677
user <mansiv@vrajatechnologies.com>
2025-12-03 10:07:12 +0530

    [UPD]product template base field.

--- qcomp_website_search/models/product_template.py
+++ qcomp_website_search/models/product_template.py
@@ -6,5 +6,8 @@ class ProductTemplate(models.Model):
     # Make /shop searches faster by not translating these fields. The
     # ORM uses CTEs to translate which cannot be efficiently
     # searched.
-    name = fields.Char(translate=False)
-    description_sale = fields.Text(translate=False)
+    name = fields.Char('Name', index='trigram', required=True, translate=True)
+    description_sale = fields.Text(
+        'Sales Description', translate=True,
+        help="A description of the Product that you want to communicate to your customers. "
+             "This description will be copied to every Sales Order, Delivery Order and Customer Invoice/Credit Note")
