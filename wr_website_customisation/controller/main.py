d87ccf84603e869c27ceb9189ec3283e0e74f767
user <mansiv@vrajatechnologies.com>
2025-12-05 17:36:18 +0530

    [UPD]quick shop module

--- wr_website_customisation/controller/main.py
+++ wr_website_customisation/controller/main.py
@@ -22,7 +22,6 @@ MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT = IMAGE_LIMITS = (1024, 768)
 LOC_PER_SITEMAP = 45000
 SITEMAP_CACHE_TIME = datetime.timedelta(hours=12)
 
-
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -31,7 +30,7 @@ class WebsiteSale(http.Controller):
 
     @http.route('/quick/shop', type='http', auth="public", website=True, sitemap=True)
     def website_quick_shop(self, **kw):
-        _logger.info("==================================website_quick_shop")
+        _logger.info("=============================quick shop")
         return request.render("wr_website_customisation.QuickShop", {})
 
     def get_filters_query(self, filter):
@@ -49,7 +48,7 @@ class WebsiteSale(http.Controller):
 
     @http.route(['/fetch_quick_shop_products'], type='json', auth="public", website=True, sitemap=False)
     def fetch_quick_shop_products(self, **post):
-        _logger.info("==================================fetch_quick_shop_products")
+        _logger.info("=============================quick shop products")
         if 'term' in post:
             query = f"""
                 select pp.id from product_product pp
@@ -59,14 +58,13 @@ class WebsiteSale(http.Controller):
                 and pp.active = 't'
                 and pt.is_published = 't'
                 and (pt.website_id = {request.website.id} or pt.website_id is null )
-                and (pt.name ILIKE '%{post.get('term')}%'
-                or pt.x_studio_alternate_number ilike '%{post.get('term')}%'
+                and (pt.name::text ilike '%{post.get('term')}%'
+                or pt.x_studio_alternate_number::text ilike '%{post.get('term')}%'
                 or pt.default_code ilike '%{post.get('term')}%') {self.get_filters_query(post.get('active_filter'))}
                 limit {post.get('limit', 20)} OFFSET {post.get('offset', 0)}
             """
             request.env.cr.execute(query)
             products_ids = request.env.cr.fetchall()
-            _logger.info("==================================products_ids %s" % products_ids)
             products_ids = [product[0] for product in products_ids]
             products_ids = request.env['product.product'].sudo().browse(products_ids)
             products = [{
@@ -88,13 +86,12 @@ class WebsiteSale(http.Controller):
                 and pp.active = 't'
                 and pt.is_published = 't'
                 and (pt.website_id = {request.website.id} or pt.website_id is null )
-                and (pt.name ILIKE '%{post.get('term')}%'
-                or pt.x_studio_alternate_number ilike '%{post.get('term')}%'
+                and (pt.name::text ilike '%{post.get('term')}%'
+                or pt.x_studio_alternate_number::text ilike '%{post.get('term')}%'
                 or pt.default_code ilike '%{post.get('term')}%')
             """)
             total_products = request.env.cr.fetchall()
             total_products = total_products[0][0]
-            _logger.info("==================================total_products %s" % total_products)
             max_offset = math.ceil(total_products / post.get('limit') or 1)
             max_offset = (max_offset - 1) * post.get('limit')
             return {
