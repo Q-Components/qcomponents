cc37acb51348bb816f55ada4f1de29427ed475ab
Aashish Jotangiya <aashishj@vrajatechnologies.com>
2025-12-08 17:16:55 +0530

    [ADD] Migrated the module of wr_listview_display_stock

--- wr_listview_display_stock/__manifest__.py
+++ wr_listview_display_stock/__manifest__.py
@@ -17,7 +17,8 @@
     "data": [],
     'assets': {
         'web.assets_backend': [
-            'wr_listview_display_stock/static/src/views/list/*',
+            '/wr_listview_display_stock/static/src/views/list/list_controller.js',
+            '/wr_listview_display_stock/static/src/views/list/list_controller.xml',
         ],
     }
 }
