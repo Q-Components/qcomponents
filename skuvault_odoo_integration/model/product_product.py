41f6218b926d67efcd910e4c3517a173bc38d0e0
Bhautik <support@vrajatechnologies.com>
2026-01-03 11:24:19 +0530

    added new fields

--- skuvault_odoo_integration/model/product_product.py
+++ skuvault_odoo_integration/model/product_product.py
@@ -23,8 +23,8 @@ class PorductProduct(models.Model):
     @api.depends('name', 'x_studio_alternate_number')
     def _compute_name_and_alternate_number(self):
         for record in self:
-            if record.x_studio_alternate_number:
-                record.name_and_alternate_number = record.name + " " + record.x_studio_alternate_number
+            record.name_and_alternate_number = record.name + " " + record.x_studio_alternate_number if record.x_studio_alternate_number else record.name
+
 
     name_and_alternate_number = fields.Char(
         string="Name and Alternate Number",
