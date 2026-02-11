3c9d86126e851c0576928084281504f03c72f247
Aashish Jotangiya <aashishj@vrajatechnologies.com>
2026-02-11 12:11:59 +0530

    [ADD] Added latest module

--- geoportal_access_control/models/website_block_ip.py
+++ geoportal_access_control/models/website_block_ip.py
@@ -15,10 +15,5 @@ class BlockedIP(models.Model):
     @api.constrains('ip_address')
     def _check_ip(self):
         for rec in self:
-            try:
-                ipaddress.ip_address(rec.ip_address)
-            except ValueError:
-                raise ValidationError("Invalid IP address.")
-                
             if self.search([('ip_address', '=', rec.ip_address), ('id', '!=', rec.id)], limit=1):
                 raise ValidationError("This IP address is already blocked.")
